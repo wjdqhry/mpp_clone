55d39ccb72512d50d11f3900d05d35216cf01ba6
bokyo <yung12320@daum.net>
2019-07-25 14:49:17 +0900

    courselist추가

---
+++ client/src/containers/CourseList.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+
+class CourseList extends Component {
+
+    render() {
+        return (
+            <div>
+                list
+            </div>
+        );
+    }
+}
+
+export default CourseList;
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,13 +1,16 @@
 import React, { Component } from "react";
 import HeaderContainer from "containers/HeaderContainer";
 import HeaderPoster from "containers/HeaderPoster";
+import CourseList from "containers/CourseList";
 
 class Home extends Component {
+
     render() {
         return (
             <>
                 <HeaderContainer />
                 <HeaderPoster />
+                <CourseList />
             </>
         );
     }
